66ce894e40c2fa53eba375fd6d6d038ec1f080d3
Genki Takiuchi <genki@s21g.com>
2008-07-11 19:35:08 +0900

    Updated README

--- lib/ns.rb
+++ lib/ns.rb
@@ -3,7 +3,7 @@ require 'singleton'
 require 'socket'
 
 class Ns
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   Listener = Struct.new(:proto, :recvq, :sendq, :laddr, :raddr, :state, :prog)
 
   include Singleton
