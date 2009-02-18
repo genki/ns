ce15772bbcc0dc2a533f28c542774a1c81e9d481
takiuchi <genki@s21g.com>
2009-02-18 23:38:58 +0900

    Bumped version.

--- lib/ns.rb
+++ lib/ns.rb
@@ -3,9 +3,7 @@ require 'singleton'
 require 'socket'
 
 class Ns
-  VERSION = "0.0.3"
-  Listener = Struct.new(:proto, :recvq, :sendq, :laddr, :raddr, :state, :prog)
-  @@all = false
+  VERSION = "0.1.0"
 
   include Singleton
 
@@ -14,43 +12,25 @@ class Ns
       action = :list
       opt = OptionParser.new
       opt.on('-k', 'kill'){action = :kill}
-      opt.on('-a', 'all'){@@all = true}
       opt.order!(ARGV)
       instance.send action, ARGV
     end
   end
 
   def list(ports)
-    @listeners.each do |l|
-      unless ports.empty?
-        next unless l.laddr.match(/[^\:]\:(?:#{ports.join('|')})\b/)
-      end
-      next if !@@all && (l.prog.nil? || l.prog == '-')
-      next if l.laddr.split(':').size > 2
-      puts "%18s  %18s  %s" % [l.laddr, l.raddr, l.prog]
-    end
+    system 'lsof', *ports.map{|i| "-i:#{i}"}
   end
 
   def kill(ports)
     puts "ports required" or exit if ports.empty?
-    @listeners.each do |l|
-      unless ports.empty?
-        next unless l.laddr.match(/[^\:]\:(?:#{ports.join('|')})\b/)
+    lines = `lsof#{ports.map{|i| " -i:#{i}"}}`.split("\n")[1..-1]
+    lines.each do |line|
+      pid = line.split(/\s+/)[1]
+      if system "kill -#{@signal || '9'} #{pid}"
+        puts "#{pid} killed."
+      else
+        puts "#{pid} not killed."
       end
-      next if l.laddr.split(':').size > 2
-      next if l.prog.nil?
-      pid, progname = l.prog.split('/', 2)
-      system "kill -9 #{pid}"
-      puts "%s(%s) killed" % [progname, pid]
-    end
-  end
-
-private
-  def initialize
-    @listeners = []
-    lines = `netstat -n -l -p 2>/dev/null`.split("\n")[1..-1]
-    lines.grep(/^(tcp|udp)/).each do |line|
-      @listeners << Listener.new(*line.split(/\s+/))
-    end
+    end unless lines.nil?
   end
 end
--- ns.gemspec
+++ ns.gemspec
@@ -1,22 +1,24 @@
+# -*- encoding: utf-8 -*-
+
 Gem::Specification.new do |s|
   s.name = %q{ns}
-  s.version = "0.0.3"
+  s.version = "0.1.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Genki Takiuchi"]
-  s.date = %q{2008-07-12}
+  s.date = %q{2009-02-18}
   s.default_executable = %q{ns}
   s.description = %q{Net}
   s.email = %q{genki@s21g.com}
   s.executables = ["ns"]
   s.extra_rdoc_files = ["README", "ChangeLog"]
-  s.files = ["README", "ChangeLog", "Rakefile", "bin/ns", "test/test_helper.rb", "test/ns_test.rb", "lib/ns.rb"]
+  s.files = ["README", "ChangeLog", "Rakefile", "bin/ns", "test/ns_test.rb", "test/test_helper.rb", "lib/ns.rb"]
   s.has_rdoc = true
   s.homepage = %q{http://ns.rubyforge.org}
   s.rdoc_options = ["--title", "ns documentation", "--charset", "utf-8", "--opname", "index.html", "--line-numbers", "--main", "README", "--inline-source", "--exclude", "^(examples|extras)/"]
   s.require_paths = ["lib"]
   s.rubyforge_project = %q{ns}
-  s.rubygems_version = %q{1.2.0}
+  s.rubygems_version = %q{1.3.1}
   s.summary = %q{Net}
   s.test_files = ["test/ns_test.rb"]
 
@@ -24,7 +26,7 @@ Gem::Specification.new do |s|
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
     s.specification_version = 2
 
-    if current_version >= 3 then
+    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<redgreen>, [">= 1.2.1"])
     else
       s.add_dependency(%q<redgreen>, [">= 1.2.1"])
